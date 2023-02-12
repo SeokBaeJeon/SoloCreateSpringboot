422623be8007ace076c5d11e39c568c0d9e51105
SeokBaeJeon <tjrqo2000@gmail.com>
2023-02-12 21:30:10 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 5장 완료

--- src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
@@ -1,5 +1,7 @@
 package com.jojoldu.book.springboot.web.dto;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -31,4 +39,5 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
+
 }
